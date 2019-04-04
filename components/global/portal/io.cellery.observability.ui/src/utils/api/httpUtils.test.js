184bbe5e606f0305f6d1c286571ce752ef0f668e
shairam <shaikrish27@gmail.com>
2019-04-04 22:38:04 +0530

    Fix npm tests

--- components/global/portal/io.cellery.observability.ui/src/utils/api/httpUtils.test.js
+++ components/global/portal/io.cellery.observability.ui/src/utils/api/httpUtils.test.js
@@ -415,12 +415,12 @@ describe("HttpUtils", () => {
 
         it(`should sign out and reject with response when axios rejects with a
             ${unauthorizedStatusCode} status code`,
-            async () => {
-                const spy = jest.spyOn(AuthUtils, "tokenRefreshRedirect");
+        async () => {
+            const spy = jest.spyOn(AuthUtils, "tokenRefreshRedirect");
 
-                await expect(mockReject(unauthorizedStatusCode)).rejects.toEqual(new Error(ERROR_DATA));
-                expect(spy).toHaveBeenCalledTimes(1);
-            });
+            await expect(mockReject(unauthorizedStatusCode)).rejects.toEqual(new Error(ERROR_DATA));
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
 
 
         it(`should only reject with response when axios rejects with a ${unauthorizedStatusCode} status code`,
