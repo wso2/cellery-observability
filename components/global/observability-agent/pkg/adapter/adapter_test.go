52b2026fe8df83c6c783db970255e6dfd75acab3
hasitha <hasithaisuru@gmail.com>
2019-11-27 14:02:22 +0530

    Change project structure and makefile

---
+++ components/global/observability-agent/pkg/adapter/adapter_test.go
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package adapter
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gogo/protobuf/types"
+	"istio.io/api/policy/v1beta1"
+	"istio.io/istio/mixer/template/metric"
+
+	"github.com/cellery-io/mesh-observability/components/global/observability-agent/pkg/logging"
+)
+
+type (
+	RoundTripFunc func(req *http.Request) *http.Response
+)
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: fn,
+	}
+}
+
+var (
+	sampleInstance1 = &metric.InstanceMsg{
+		Name: "wso2spadapter-metric",
+		Dimensions: map[string]*v1beta1.Value{
+			"response_ip": {
+				Value: &v1beta1.Value_IpAddressValue{IpAddressValue: &v1beta1.IPAddress{Value: []byte{}}},
+			},
+		},
+		Value: &v1beta1.Value{
+			Value: &v1beta1.Value_Int64Value{Int64Value: 350},
+		},
+	}
+	sampleInstance2 = &metric.InstanceMsg{
+		Name: "wso2spadapter-metric",
+		Dimensions: map[string]*v1beta1.Value{
+			"response_bool": {
+				Value: &v1beta1.Value_BoolValue{BoolValue: false},
+			},
+		},
+		Value: &v1beta1.Value{
+			Value: &v1beta1.Value_Int64Value{Int64Value: 0},
+		},
+	}
+	sampleInstance3 = &metric.InstanceMsg{
+		Name: "wso2spadapter-metric",
+		Dimensions: map[string]*v1beta1.Value{
+			"response_string": {
+				Value: &v1beta1.Value_StringValue{StringValue: "Test"},
+			},
+		},
+		Value: &v1beta1.Value{
+			Value: &v1beta1.Value_Int64Value{Int64Value: 0},
+		},
+	}
+	sampleInstance4 = &metric.InstanceMsg{
+		Name: "wso2spadapter-metric",
+		Dimensions: map[string]*v1beta1.Value{
+			"response_double": {
+				Value: &v1beta1.Value_DoubleValue{DoubleValue: 1.5},
+			},
+		},
+		Value: &v1beta1.Value{
+			Value: &v1beta1.Value_Int64Value{Int64Value: 0},
+		},
+	}
+	sampleInstance5 = &metric.InstanceMsg{
+		Name: "wso2spadapter-metric",
+		Dimensions: map[string]*v1beta1.Value{
+			"response_code": {
+				Value: &v1beta1.Value_Int64Value{Int64Value: 200},
+			},
+		},
+		Value: &v1beta1.Value{
+			Value: &v1beta1.Value_Int64Value{Int64Value: 0},
+		},
+	}
+	sampleInstance6 = &metric.InstanceMsg{
+		Name: "wso2spadapter-metric",
+		Dimensions: map[string]*v1beta1.Value{
+			"response_duration": {
+				Value: &v1beta1.Value_DurationValue{DurationValue: &v1beta1.Duration{Value: &types.Duration{Nanos: 200}}},
+			},
+		},
+		Value: &v1beta1.Value{
+			Value: &v1beta1.Value_Int64Value{Int64Value: 0},
+		},
+	}
+)
+
+func TestNewAdapter(t *testing.T) {
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	buffer := make(chan string, 100)
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: 200,
+			Header:     make(http.Header),
+		}
+	})
+	mixer := &Mixer{&TLS{}}
+	adapter, err := New(AdapterPort, logger, client, buffer, mixer)
+	expectedStr := fmt.Sprintf("[::]:%d", AdapterPort)
+	if err != nil {
+		t.Errorf("Error while creating the adapter : %v", err)
+	}
+	if adapter.Addr() == expectedStr {
+		defer func() {
+			err := adapter.Close()
+			if err != nil {
+				log.Fatalf("Error closing adapter: %v", err)
+			}
+		}()
+		t.Log("Success, expected address has received")
+	} else {
+		t.Error("Fail, Expected address has not received")
+	}
+}
+
+func TestNewAdapterWithInvalidTlsData(t *testing.T) {
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	buffer := make(chan string, 100)
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: 200,
+			Header:     make(http.Header),
+		}
+	})
+	tls := &Mixer{TLS: &TLS{
+		Certificate:   "./testdata/test.crt",
+		PrivateKey:    "./testdata/test.key",
+		CaCertificate: "./testdata/test.pem",
+	}}
+	adapter, err := New(AdapterPort, logger, client, buffer, tls)
+	if adapter == nil {
+		t.Error("Received struct of the adapter is null")
+	}
+	_ = adapter.Close()
+}
+
+func TestNewAdapterWithTLS(t *testing.T) {
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	buffer := make(chan string, 100)
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: 200,
+			Header:     make(http.Header),
+		}
+	})
+	tls := &Mixer{TLS: &TLS{
+		Certificate:   "./testdata/adapter.crt",
+		PrivateKey:    "./testdata/adapter.key",
+		CaCertificate: "./testdata/ca.pem",
+	}}
+	adapter, err := New(AdapterPort, logger, client, buffer, tls)
+	if adapter == nil {
+		t.Error("Received struct of the adapter is null")
+	}
+	_ = adapter.Close()
+}
+
+func TestHandleMetric(t *testing.T) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", AdapterPort))
+	if err != nil {
+		t.Errorf("Unable to listen on socket: %v", err)
+	}
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.WriteHeader(200)
+	}))
+	defer func() { testServer.Close() }()
+	buffer := make(chan string, 100)
+	wso2SpAdapter := &Adapter{
+		listener:   listener,
+		logger:     logger,
+		httpClient: &http.Client{},
+		buffer:     buffer,
+	}
+	var sampleInstances []*metric.InstanceMsg
+	sampleInstances = append(sampleInstances, sampleInstance1)
+	sampleInstances = append(sampleInstances, sampleInstance2)
+	sampleInstances = append(sampleInstances, sampleInstance3)
+	sampleInstances = append(sampleInstances, sampleInstance4)
+	sampleInstances = append(sampleInstances, sampleInstance5)
+	sampleInstances = append(sampleInstances, sampleInstance6)
+	sampleMetricRequest := metric.HandleMetricRequest{
+		Instances: sampleInstances,
+	}
+	_, err = wso2SpAdapter.HandleMetric(context.TODO(), &sampleMetricRequest)
+	if err != nil {
+		t.Errorf("Metrics could not be handled : %v", err)
+	}
+}
---
+++ components/global/observability-agent/pkg/store/file/file_test.go
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package file
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gofrs/flock"
+
+	"github.com/cellery-io/mesh-observability/components/global/observability-agent/pkg/logging"
+)
+
+var (
+	testStr = "{\"contextReporterKind\":\"inbound\", \"destinationUID\":\"kubernetes://istio-policy-74d6c8b4d5-mmr49.istio-system\", \"requestID\":\"6e544e82-2a0c-4b83-abcc-0f62b89cdf3f\", \"requestMethod\":\"POST\", \"requestPath\":\"/istio.mixer.v1.Mixer/Check\", \"requestTotalSize\":\"2748\", \"responseCode\":\"200\", \"responseDurationNanoSec\":\"695653\", \"responseTotalSize\":\"199\", \"sourceUID\":\"kubernetes://pet-be--controller-deployment-6f6f5768dc-n9jf7.default\", \"spanID\":\"ae295f3a4bbbe537\", \"traceID\":\"b55a0f7f20d36e49f8612bac4311791d\"}"
+)
+
+func TestWriteWithoutErrors(t *testing.T) {
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	persister := &Persister{
+		logger:    logger,
+		directory: ".",
+	}
+	err = persister.Write(fmt.Sprintf("[%s]", testStr))
+	if err != nil {
+		t.Error("Could not write")
+	}
+	files, err := filepath.Glob("./*.json")
+	for _, fname := range files {
+		err = os.Remove(fname)
+	}
+}
+
+func TestWriteWithInvalidDirectory(t *testing.T) {
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	persister := &Persister{
+		logger:    logger,
+		directory: "./wrong_directory",
+	}
+	err = persister.Write(fmt.Sprintf("[%s]", testStr))
+	expectedErr := "no such file or directory"
+	if err == nil {
+		t.Errorf("An error was not thrown, but expected : %s", expectedErr)
+		return
+	}
+	if strings.Contains(expectedErr, err.Error()) {
+		t.Errorf("Expected error was not thrown, received error : %v", err)
+	}
+	files, err := filepath.Glob("./*.json")
+	for _, fname := range files {
+		err = os.Remove(fname)
+	}
+}
+
+func TestFetchWithoutErrors(t *testing.T) {
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	persister := &Persister{
+		logger:    logger,
+		directory: "./",
+	}
+	_ = ioutil.WriteFile("./test.json", []byte(testStr), 0644)
+	str, _, _ := persister.Fetch()
+	if str != testStr {
+		t.Error("Contents are not equal")
+	}
+	files, err := filepath.Glob("./*.json")
+	for _, fname := range files {
+		err = os.Remove(fname)
+	}
+}
+
+func TestFetchWithEmptyFile(t *testing.T) {
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	persister := &Persister{
+		logger:    logger,
+		directory: "./",
+	}
+	_ = ioutil.WriteFile("./test.json", []byte(""), 0644)
+	_, _, err = persister.Fetch()
+	expectedErr := "file is empty, hence removed"
+	if err == nil {
+		t.Errorf("An error was not thrown, but expected : %s", expectedErr)
+		return
+	}
+	if err.Error() != expectedErr {
+		t.Errorf("Expected error was not thrown, received error : %v", err)
+	}
+	files, err := filepath.Glob("./*.json")
+	for _, fname := range files {
+		err = os.Remove(fname)
+	}
+}
+
+func TestFetchWithInvalidDirectory(t *testing.T) {
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	persister := &Persister{
+		logger:    logger,
+		directory: "./wrong_dir",
+	}
+	str, _, err := persister.Fetch()
+	if str != "" {
+		t.Error("Unexpected behaviour from the function")
+	}
+}
+
+func TestCommitWithoutErrors(t *testing.T) {
+	_ = ioutil.WriteFile("./test.json", []byte(testStr), 0644)
+	transaction := &Transaction{
+		Lock: flock.New("./test.json"),
+	}
+	err := transaction.Commit()
+	if err != nil {
+		t.Errorf("Error when committing : %v", err)
+	}
+	files, err := filepath.Glob("./*.json")
+	for _, fname := range files {
+		err = os.Remove(fname)
+	}
+}
+
+func TestCommitWithError(t *testing.T) {
+	transaction := &Transaction{
+		Lock: flock.New("./test.json"),
+	}
+	err := transaction.Commit()
+	expectedErr := "could not delete the published file : remove ./test.json: no such file or directory"
+	if err == nil {
+		t.Errorf("An error was not thrown, but expected : %s", expectedErr)
+		return
+	}
+	if err.Error() != expectedErr {
+		t.Errorf("Expected error was not thrown, received error : %v", err)
+	}
+}
+
+func TestRollback(t *testing.T) {
+	_ = ioutil.WriteFile("./test.json", []byte(testStr), 0644)
+	transaction := &Transaction{
+		Lock: flock.New("./test.json"),
+	}
+	_, _ = transaction.Lock.TryLock()
+	err := transaction.Rollback()
+	if err != nil {
+		t.Errorf("An error was thrown when unlocking the file : %v", err)
+	}
+	files, err := filepath.Glob("./*.json")
+	for _, fname := range files {
+		err = os.Remove(fname)
+	}
+}
+
+func TestNewMethodWithoutDir(t *testing.T) {
+	config := &File{Path: "./testDir"}
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	_, _ = NewPersister(config, logger)
+	_, err = os.Stat(config.Path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			t.Error("Directory has not been created")
+		} else {
+			t.Errorf("An unexpected error has been occured : %v", err)
+		}
+	}
+	_ = os.RemoveAll(config.Path)
+}
+
+func TestNewMethodWithDir(t *testing.T) {
+	config := &File{Path: "./testDir"}
+	err := os.MkdirAll(config.Path, os.ModePerm)
+	if err != nil {
+		t.Errorf("error occurred when creating the directory : %v", err)
+	}
+	logger, err := logging.NewLogger()
+	if err != nil {
+		t.Errorf("Error building logger: %v", err)
+	}
+	_, err = NewPersister(config, logger)
+	if err != nil {
+		t.Errorf("An unexpected error has been occured : %v", err)
+	}
+	_ = os.RemoveAll(config.Path)
+}
